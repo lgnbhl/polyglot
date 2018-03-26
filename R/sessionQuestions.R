e094738256e8637971fd2128dbacb5e7240b3d4e
Félix Luginbühl <felix.luginbuhl@protonmail.ch>
2018-03-26 19:02:15 +0200

    Update sessionQuestions.R

--- R/sessionQuestions.R
+++ R/sessionQuestions.R
@@ -40,8 +40,7 @@ sessionQuestions <- function(assign.env = parent.frame(1)) {
             }
            assign("sessionDataset", sessionDataset, envir = assign.env)
           },
-          sessionDataset$Score[1] <- sessionDataset$Score[1] + 2
-          assign("sessionDataset", sessionDataset, envir = assign.env),
+          sessionDataset$Score[1] <- sessionDataset$Score[1] + 2,
           if(sessionDataset$Repetition[1] == 0) {
                sessionDataset$Score[1] <- sessionDataset$Score[1] + 4
                assign("sessionDataset", sessionDataset, envir = assign.env)
