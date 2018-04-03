ce30829b29d45d7a60fc9ef131885a48b4b2aac5
Félix Luginbühl <felix.luginbuhl@protonmail.ch>
2018-04-03 15:34:44 +0200

    Update polyglot 0.2

--- R/learn.R
+++ R/learn.R
@@ -32,10 +32,14 @@
 learn <- function(assign.env = parent.frame(1)) {
   cat("| Welcome to polyglot! \n")
   cat("| Please choose a dataset to study, or type 0 to exit. \n")
+  
+  # add sessionStartTime object if not existing
   if(!exists("sessionStartTime")) {
     sessionStartTime <- Sys.time()
     assign("sessionStartTime", sessionStartTime, envir = assign.env)
   }
+  
+  # interactive selection of a dataset
   datasetPath <- system.file("extdata/", package = "polyglot")
   datasetName <- select.list(list.files(path = paste0("", datasetPath, "")))
   datasetAbsolutePath <- paste0("", datasetPath,"/", datasetName,"")
@@ -45,6 +49,7 @@ learn <- function(assign.env = parent.frame(1)) {
   } else {
     sessionDataset <- read.csv(paste0("", datasetAbsolutePath,""), stringsAsFactors = FALSE)
     assign("sessionDataset", sessionDataset, envir = assign.env)
+    
     # Check if the dataset has at least two colomns
     if (ncol(sessionDataset) < 2) {
       message("| WARNING: Your dataset doesn't have two colomns.")
@@ -56,6 +61,7 @@ learn <- function(assign.env = parent.frame(1)) {
       sessionDataset[,2] <- as.character(sessionDataset[,2])
       assign("sessionDataset", sessionDataset, envir = assign.env)
     }
+        
     # Add Score variable if not existing and print message
     if (any(names(sessionDataset) == "Score")) {
       cat(paste("|", datasetName, "selected, with", nrow(sessionDataset),"rows. \n"))
@@ -68,28 +74,59 @@ learn <- function(assign.env = parent.frame(1)) {
     }
     # If NAs exist in the Score variable, replace by 0
     sessionDataset$Score[is.na(sessionDataset$Score)] <- 0
-    # Add Date variable if not existing and print message
-    if (any(names(sessionDataset) == "Date")) {
-      cat(paste("|", length(which(sessionDataset$Date <= Sys.Date())),"rows left to learn. \n\n"))
-        if(length(which(sessionDataset$Date <= Sys.Date())) == 1) {
-        cat(paste("| 1 row left to learn. \n\n"))
-        }
-      } else {
-      sessionDataset$Date <- rep(Sys.Date(), nrow(sessionDataset)) # add today date
-      assign("sessionDataset", sessionDataset, envir = assign.env)
-      write.csv(sessionDataset, file = paste0("", datasetAbsolutePath, ""), row.names = FALSE)
-    }
-    # if NAs exist in the Date variable, replace by today
-    sessionDataset$Date[is.na(sessionDataset$Date)] <- Sys.Date()
-    # Add Repetition variable if not existing
-    if (any(names(sessionDataset) != "Repetition")) {
-      sessionDataset$Repetition <- rep(as.numeric(0), nrow(sessionDataset))
-      assign("sessionDataset", sessionDataset, envir = assign.env)
-      write.csv(sessionDataset, file = paste0("", datasetAbsolutePath, ""), row.names = FALSE)
-    }
-    # if NAs exist in the Repetition variable, replace by 0
-    sessionDataset$Repetition[is.na(sessionDataset$Repetition)] <- 0
     assign("sessionDataset", sessionDataset, envir = assign.env)
+    
+    # Add dueDate variable if not existing and print message
+    if (any(names(sessionDataset) == "dueDate")) {
+        cat(paste("|", length(which(sessionDataset$dueDate <= as.Date(Sys.Date()))),"rows left to learn. \n\n"))
+         if(length(which(sessionDataset$dueDate <= as.Date(Sys.Date()))) == 1) {
+            cat(paste("| 1 row left to learn. \n\n"))
+         }
+    } else {
+      sessionDataset$dueDate <- rep(as.Date(Sys.Date()), nrow(sessionDataset)) # add today date
+      assign("sessionDataset", sessionDataset, envir = assign.env)
+      write.csv(sessionDataset, file = paste0("extdata/", datasetName), row.names = FALSE)
+    }
+    # If NAs exist in the dueDate variable, replace by today date
+    sessionDataset$dueDate[is.na(sessionDataset$dueDate)] <- as.Date(Sys.Date())
+    assign("sessionDataset", sessionDataset, envir = assign.env)
+    
+    # Add numeric Repetition variable if not existing
+    if (any(names(sessionDataset) == "Repetition")) {
+      # Repetition already exists
+    } else {
+      sessionDataset$Repetition <- rep(as.numeric(0), nrow(sessionDataset)) # add today date
+      assign("sessionDataset", sessionDataset, envir = assign.env)
+      write.csv(sessionDataset, file = paste0("extdata/", datasetName), row.names = FALSE)
+    }
+    # If NAs exist in the Repetition variable, replace by 0
+    sessionDataset$Repetition[is.na(sessionDataset$Repetition)] <- as.numeric(0)
+    assign("sessionDataset", sessionDataset, envir = assign.env)
+    
+    # Add numeric easiness factor (eFactor) if not existing
+    if (any(names(sessionDataset) == "eFactor")) {
+      # eFactor already exists
+    } else {
+      sessionDataset$eFactor <- rep(as.numeric(2.5), nrow(sessionDataset))
+      assign("sessionDataset", sessionDataset, envir = assign.env)
+      write.csv(sessionDataset, file = paste0("extdata/", datasetName), row.names = FALSE)
+    }
+    # If NAs exist in the eFactor variable, replace by 2.5
+    sessionDataset$eFactor[is.na(sessionDataset$eFactor)] <- as.numeric(2.5)
+    assign("sessionDataset", sessionDataset, envir = assign.env)
+    
+    # Add difftime Interval object if not existing
+    if (any(names(sessionDataset) == "Interval")) {
+      # Interval already exists
+    } else {
+      sessionDataset$Interval <-rep(as.difftime(0, units = "days"), nrow(sessionDataset))
+      assign("sessionDataset", sessionDataset, envir = assign.env)
+      write.csv(sessionDataset, file = paste0("extdata/", datasetName), row.names = FALSE)
+    }
+    # If NAs exist in the Interval variable, replace by 0
+    sessionDataset$Interval[is.na(sessionDataset$Interval)] <- as.difftime(0, units = "days")
+    assign("sessionDataset", sessionDataset, envir = assign.env)
+    
     write.csv(sessionDataset, file = paste0("", datasetAbsolutePath, ""), row.names = FALSE)
     return(sessionQuestions())
   }
